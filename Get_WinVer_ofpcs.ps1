42fe1f86f21cc1d6580b4c05dbe2ec2445328388
martin.hartung <martin.hartung2019@outlook.com>
2023-04-12 17:00:10 +0200

    added infos to readme

---
+++ Get_WinVer_ofpcs.ps1
@@ -0,0 +1,32 @@
+# Add PCs here
+$pc = @'
+PC
+ADD PC NAMES HERE
+'@ | ConvertFrom-Csv
+
+#output variable
+$output = @()
+
+#for each PC in the list get Windows Version and Current User and put append it as object
+foreach($item in $pc.PC)
+{
+if((Get-WmiObject Win32_OperatingSystem -ComputerName $item).Version)
+{
+    $output += New-Object -TypeName Psobject -Property @{
+        PC = $item
+        Verion = (Get-WmiObject Win32_OperatingSystem -ComputerName $item).Version
+        CurrentUser = (Get-WmiObject -ComputerName $item -Class Win32_ComputerSystem).UserName
+    }
+}
+else
+{
+    $output += New-Object -TypeName psobject -Property @{
+        PC = $item
+        Verion = "nicht erreichbar"
+        CurrentUser = ""
+    }
+}
+}
+
+#write output into an excelfile 
+$output | Export-Excel -Path C:\temp\windreamwinver.xlsx
---
+++ readme.txt
@@ -0,0 +1,14 @@
+### This script takes a PC names and look up the win version of them       ####
+### the out put is stored in an excel .xlsx in C:\temp\windreamwinver.xlsx ####
+
+## Runtime: PS v5.2+
+
+#### dependencies ####
+
+To export in excel
+https://www.powershellgallery.com/packages/ImportExcel/7.4.1
+
+
+# find windows builds here #
+
+https://learn.microsoft.com/de-de/windows/release-health/release-information
